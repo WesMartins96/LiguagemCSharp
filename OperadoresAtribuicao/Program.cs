9ff97b99ab87f3362d392409b8859706a505d0f5
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-20 01:52:02 -0300

    operadores de atribuicao

---
+++ OperadoresAtribuicao/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace OperadoresAtribuicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+
+            a += 2;
+            Console.WriteLine(a);
+
+            a *= 3; 
+            Console.WriteLine(a);
+
+            string s = "abc";
+            Console.WriteLine(s.ToUpper());
+            s += "def";
+            Console.WriteLine(s.ToUpper());
+
+
+            //Operadores aritméticos / atribuição
+            int num1 = 10;
+            num1++;
+            Console.WriteLine(num1);
+
+            int num2 = 10;
+            num2--;
+            Console.WriteLine(num2);
+
+            int x = 10;
+            int y = ++x;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
