df95f2480730e5d4addb827fc1dc43863c9754fc
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-19 21:32:43 -0300

    Adicionar arquivos de projeto.

---
+++ SaidaDeDados/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDeDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Mudar de ',' para '.' em numeros decimais
+            var culture = CultureInfo.InvariantCulture;
+
+            Console.Write("Bom Dia!");
+            Console.WriteLine("Boa Tarde!");
+            Console.WriteLine("Boa Noite!");
+            Console.WriteLine("--------------");
+
+            char genero = 'F';
+            int idade = 28;
+            double saldo = 10.35784;
+            string nome = "Wesley";
+
+            Console.WriteLine(genero);
+            Console.WriteLine(idade);
+            Console.WriteLine(saldo.ToString("F2", culture));
+            Console.WriteLine(saldo.ToString("F4", culture));
+            Console.WriteLine(nome);
+            Console.WriteLine("--------------");
+
+
+            //Placeholders, interpolação e concatenação de strings
+            Console.WriteLine("{0} tem {1} anos e tem saldo atual de: R${2}", nome, idade, saldo.ToString("F2", culture));
+
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo atual de: {saldo:C}");
+
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo atual de: R$" + saldo.ToString("F3", culture));
+
+        }
+    }
+}
---
+++ TiposBasicos/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TiposBasicos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool completo = false;
+            Console.WriteLine(completo);
+
+            char genero = 'F';
+            char unicode = '\u0041';
+            Console.WriteLine(unicode);
+            Console.WriteLine(genero);
+
+            sbyte x = 100;
+            Console.WriteLine(x);
+
+            byte n1 = 126;
+            Console.WriteLine(n1);
+
+            int n2 = 2147483647;
+            Console.WriteLine(n2);
+
+            long n3 = 2147483648L;
+            Console.WriteLine(n3);
+
+            float n5 = 4.5f;
+            Console.WriteLine(n5);
+
+            double n6 = 4.8;
+            Console.WriteLine(n6);
+
+            string nome = "Wesley Martins Desenvolvedor";
+            Console.WriteLine(nome);
+
+            object obj1 = "Gael Martins";
+            Console.WriteLine(obj1);
+
+            object obj2 = 5.5;
+            Console.WriteLine(obj2);
+
+            decimal n7 = decimal.MaxValue;
+            Console.WriteLine(n7);
+
+            Console.ReadKey();
+
+        }
+    }
+}
