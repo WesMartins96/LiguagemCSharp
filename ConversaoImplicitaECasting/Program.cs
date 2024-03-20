b90c81e372ff4f5cc24271b24abe0efd6ddcf66e
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-20 02:03:42 -0300

    Conversao implicita e Casting

---
+++ ConversaoImplicitaECasting/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConversaoImplicitaECasting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Conversão implicíta
+            float x = 4.5f;
+            double y = x;
+            Console.WriteLine(y);
+
+
+            //Casting (Possível perda de informação)
+            double a = 5.1;
+            float b = (float)a;
+            Console.WriteLine(b);
+
+
+            //Perda de informação no casting de numero decimal para inteiro
+            double dnum = 6.5;
+            int inum = (int)dnum;
+            Console.WriteLine(inum);
+
+
+            //Casting no result para não perder informação
+            int ex1 = 5;
+            int ex2 = 2;
+            double result = (double) ex1 / ex2;
+            Console.WriteLine(result);
+
+        }
+    }
+}
