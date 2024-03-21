272d21ce29396e68a76e697539488ae9f764741f
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-20 22:12:03 -0300

    Estrutura Repetitiva For

---
+++ EstruturaRepetitivaFor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EstruturaRepetitivaFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Digitar um número N e depois N valores inteiros. Mostrar a soma dos N valores digitados.
+            Console.Write("Quantos números inteiros você vai digitar? ");
+            int n = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"VALOR #{i}: ");
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+
+            Console.WriteLine($"Soma = {soma}");
+        }
+    }
+}
