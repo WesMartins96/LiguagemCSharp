4d956984590ddf9551f556bbde24ac103319dcc8
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-20 21:04:29 -0300

    um pouco sobre funcoes

---
+++ Funcoes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Funcoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite 3 números e veja qual o maior entre eles:");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            //Criar uma função para verificar qual o número maior
+            int resultado = Maior(n1, n2, n3);
+            Console.WriteLine($"O maior número entre eles é o: {resultado}");
+        }
+
+        static int Maior(int n1, int n2, int n3)
+        {
+            int m;
+            if (n1 > n2 && n1 > n3)
+            {
+                m = n1; ;
+            }
+            else if (n2 > n3)
+            {
+                m = n2;
+            }
+            else
+            {
+                m = n3;
+            }
+
+            return m;
+        }
+    }
+}
