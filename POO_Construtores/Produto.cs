9fa8f4885fc56ea75fc2b74f4bf186eaf7cffd52
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-22 12:18:49 -0300

    POO Construtores e sobrecargas

---
+++ POO_Construtores/Produto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Construtores
+{
+    class Produto
+    {
+
+        public string Nome;
+        public double Preco;
+        public int Estoque;
+
+        //Construtor
+        public Produto(string nome, double preco, int estoque)
+        {
+            Nome = nome;
+            Preco = preco;
+            Estoque = estoque;
+        }
+
+
+        //Sobrecarga
+        public Produto(string nome, double preco) 
+        {
+            Nome = nome;
+            Preco = preco;
+            //poderiamos retirar a linha abaixo, pois por padrao os atributos numericos recebem o valor zero.
+            Estoque = 0;
+        }    
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Estoque;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Estoque += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Estoque -= quantidade;
+        }
+
+        CultureInfo culture = CultureInfo.InvariantCulture;
+        //Converter objeto em string
+        public override string ToString()
+        {
+            return string.Format($"Dados do produto: {Nome}, $ {Preco.ToString("F2", culture)}, {Estoque} unidades, Total: $ {ValorTotalEmEstoque().ToString("F2", culture)}");
+        }
+
+
+    }
+}
---
+++ POO_Construtores/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO_Construtores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var culture = CultureInfo.InvariantCulture;
+            
+
+            Console.WriteLine("Entre com os dados do produto:");
+
+            Console.Write("Nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            double preco = double.Parse(Console.ReadLine(), culture);
+
+            
+            Produto produto = new Produto(nome, preco);
+
+            Console.WriteLine(produto);
+
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser adicionado ao estoque: ");
+            int quantidade = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(quantidade);
+
+            Console.WriteLine("Dados atualizados: " + produto);
+
+            Console.WriteLine();
+
+            Console.Write("Digite o número de produtos a ser removido do estoque: ");
+            quantidade = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(quantidade);
+
+            Console.WriteLine("Dados atualizados: " + produto);
+
+        }
+    }
+}
