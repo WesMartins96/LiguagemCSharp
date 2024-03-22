18e30c1984838f2380be8a50c5f03f3f94399002
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-22 19:35:25 -0300

    POO properties e auto properties

---
+++ POO_Properties/Produto.cs
@@ -0,0 +1,68 @@
+﻿using System.Globalization;
+
+namespace POO_Properties
+{
+    class Produto
+    {
+        //Ordem sugerida para implementação de membros    
+        //1) Atributos privados
+        private string _nome;
+
+        //2) Propriedades auto-implementadas
+        //Auto properties
+        public double Preco { get; private set; }
+        public int Estoque { get; private set; }
+
+        //3) Construtores
+        public Produto() { }
+
+        public Produto(string nome, double preco, int estoque)
+        {
+            _nome = nome;
+            Preco = preco;
+            Estoque = estoque;
+        }
+
+
+        //4) Propriedades customizada
+        //Properties
+        public string Nome
+        {
+            //Get e Set usando properties, não usaremos o auto properties pois no set contem uma regra na lógica.
+            get { return _nome; }
+            set
+            {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        //5) Outros métodos da classe
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Estoque;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            Estoque += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            Estoque -= quantidade;
+        }
+
+        CultureInfo culture = CultureInfo.InvariantCulture;
+        public override string ToString()
+        {
+            return string.Format($"Dados do produto: {_nome}, $ {Preco.ToString("F2", culture)}, {Estoque} unidades, Total: $ {ValorTotalEmEstoque().ToString("F2", culture)}");
+        }
+
+
+
+
+    }
+}
---
+++ POO_Properties/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO_Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var culture = CultureInfo.InvariantCulture;
+
+            Produto produto = new Produto("TV", 500.00, 10);
+
+            Console.WriteLine(produto.Nome);
+            produto.Nome = "Playstation 5";
+
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine(produto.Preco.ToString("F2", culture));
+            Console.WriteLine(produto.Estoque);
+        }
+    }
+}
