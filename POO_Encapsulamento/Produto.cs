5bfb788dd11e9ba757bf578e18fa1ab00a65d30e
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-22 13:46:22 -0300

    POO encapsulamento com Get e Set

---
+++ POO_Encapsulamento/Produto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Encapsulamento
+{
+    class Produto
+    {
+
+        //quando usamos private o nome muda para _ no inicio exemplos abaixo:
+        //para disponibilizarmos no programa usamos get e set para os atributos
+        private string _nome;
+        private double _preco;
+        private int _estoque;
+
+        //Get para disponibilizar o atributo private (_nome)
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        //Set para ter acesso a alteração do atributo e colocando lógica encapsulando
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1)
+            {
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetEstoque()
+        {
+            return _estoque;
+        }
+
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco, int estoque)
+        {
+            _nome = nome;
+            _preco = preco;
+            _estoque = estoque;
+        }
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _estoque;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _estoque += quantidade;
+        }
+
+        public void RemoverProdutos(int quantidade)
+        {
+            _estoque -= quantidade;
+        }
+
+        CultureInfo culture = CultureInfo.InvariantCulture;
+        public override string ToString()
+        {
+            return string.Format($"Dados do produto: {_nome}, $ {_preco.ToString("F2", culture)}, {_estoque} unidades, Total: $ {ValorTotalEmEstoque().ToString("F2", culture)}");
+        }
+
+
+    }
+}
---
+++ POO_Encapsulamento/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace POO_Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var culture = CultureInfo.InvariantCulture;
+            
+            Produto produto = new Produto("TV", 500.00, 10);
+
+            Console.WriteLine(produto.GetNome());
+            produto.SetNome("Playstation 5");
+
+            Console.WriteLine(produto.GetNome());
+            Console.WriteLine(produto.GetPreco().ToString("F2", culture));
+            Console.WriteLine(produto.GetEstoque());
+
+        }
+    }
+}
