99211456f24582636b8ea542007089e4c5cbf60b
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-23 01:16:13 -0300

    POO vetores de tipo referencia e tipo valor

---
+++ Vetores/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var culture = CultureInfo.InvariantCulture;
+            //Vetores
+            int n = int.Parse(Console.ReadLine());
+
+            //Criar Vetor e instancia a partir dos numeros digitados pelo usuário n;
+            double[] vect = new double[n];
+
+            //para percorrer um vetor usa-se for
+            for (int i = 0; i < n; i++)
+            {
+                vect[i] = double.Parse(Console.ReadLine(), culture);
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i];
+            }
+
+            double alturaMedia = soma / n;
+            Console.WriteLine($"Altura média: {alturaMedia.ToString("F2", culture)}");
+
+        }
+    }
+}
---
+++ VetoresII/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace VetoresII
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var culture = CultureInfo.InvariantCulture; 
+            //Vetores 
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] produtos = new Produto[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), culture);
+
+                produtos[i] = new Produto
+                {
+                    Nome = nome,
+                    Preco = preco
+                };
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                soma += produtos[i].Preco;
+            }
+
+            double media = soma / n;
+            Console.WriteLine($"Preço médio: {media.ToString("F2", culture)}");
+        }
+    }
+}
