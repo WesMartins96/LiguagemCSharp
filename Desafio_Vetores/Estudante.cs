65bb9bd82c206aa4e4c85d76e9051065213d7c59
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-24 01:37:04 -0300

    Desafio vetores com classe

---
+++ Desafio_Vetores/Estudante.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Vetores
+{
+    class Estudante
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudante(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"{Nome}, {Email}");
+        }
+
+    }
+}
---
+++ Desafio_Vetores/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Desafio_Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exercicio 1
+            /* 
+             A dona de um pensionato possui dez quartos para alugar para estudantes, sendo esses quartos identificados pelo números 0 a 9.
+
+            Fazer um programa que inicie com todos os dez quartos vazios, e depois leia a quantidade N representando o número de estudantes 
+            que vão alugar quartos (1 a 10). Em seguida, registre o aluguel dos N estudantes, Para cada registro de aluguel, informar o nome e email do estudante,
+            bem como qual dos quartos ele escolheu (de 0 a 9). Suponha que seja escolhido um quarto vago. Ao final, seu programa deve imprimir um relatório de
+            todas ocupações do pensionato, por ordem de quarto.
+             */
+            Console.WriteLine("--- Exercício 1 ---");
+
+            Estudante[] estudantes = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados? ");
+            int n = int.Parse(Console.ReadLine());
+  
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Aluguel #{i}");
+
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                estudantes[quarto] = new Estudante(nome, email);
+                
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Quartos Ocupados: ");
+            for (int i = 0; i < 10; i++)
+            {
+                if (estudantes[i] != null)
+                {
+                    Console.WriteLine($"{i}: {estudantes[i]}");
+                }
+            }
+
+            #endregion
+
+
+        }
+    }
+}
