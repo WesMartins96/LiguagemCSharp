973ec3ff6558a0d016185f0ba530d01539ae6de5
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-25 08:15:15 -0300

    enumeracao e conversao de string e vice versa

---
+++ POO_Enumeracao/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace POO_Enumeracao.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment = 0,
+        Processing = 1,
+        Shipped = 2,
+        Delivered = 3
+    }
+}
---
+++ POO_Enumeracao/Program.cs
@@ -0,0 +1,30 @@
+﻿using POO_Enumeracao.Entities;
+using POO_Enumeracao.Entities.Enums;
+using System;
+
+namespace POO_Enumeracao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+
+            Console.WriteLine(order);
+
+            //Conversão de enumeração para string 
+            string txt = OrderStatus.PendingPayment.ToString();
+            Console.WriteLine(txt);
+
+            //Conversão de string para enumeração 
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            Console.WriteLine(os);
+
+        }
+    }
+}
