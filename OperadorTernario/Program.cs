6c2f72460084219ae5fe2c36d702db74d67db41e
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-25 00:55:55 -0300

    operador ternario para substituicao de alguns if/else

---
+++ OperadorTernario/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+
+namespace OperadorTernario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var culture = CultureInfo.InvariantCulture;
+            double preco = double.Parse(Console.ReadLine(), culture);
+
+            double desconto;
+
+            //usando operador ternário
+            desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+
+            //Sem usar operador ternário
+            //if (preco < 20.0)
+            //{
+            //    desconto = preco * 0.1; //10%
+            //}
+            //else
+            //{
+            //    desconto = preco * 0.05; //5%
+            //}
+
+            Console.WriteLine(desconto);
+        }
+    }
+}
