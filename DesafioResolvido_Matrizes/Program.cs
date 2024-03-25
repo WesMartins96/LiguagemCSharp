61b293ad182d9d27615f14837f862680983389aa
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-25 00:18:31 -0300

    exemplo de como podemos usar matrizes

---
+++ DesafioResolvido_Matrizes/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DesafioResolvido_Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exercicio Resolvido 
+            /*
+            Fazer um programa para ler um número inteiro N e uma matriz de
+            ordem N contendo números inteiros. Em seguida, mostrar a diagonal
+            principal e a quantidade de valores negativos da matriz.
+            */
+            Console.WriteLine("--- Exercício Resolvido");
+            Console.WriteLine();
+
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] mat = new int[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] valores = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < n; j++)
+                {
+                    mat[i, j] = int.Parse(valores[j]);
+                }
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("Main Diagonal:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(mat[i, i] + " ");
+            }
+
+            Console.WriteLine();
+
+            int contagem = 0;
+            //Estrutura básica para se percorrer uma matriz inteira
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (mat[i, j] < 0)
+                    {
+                        contagem++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Negative numbers: {contagem}");
+            #endregion
+        }
+    }
+}
---
+++ Matrizes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declarar uma matrix, arranjo bi dimensional ((linhas →)  e (colunas ↓)) 
+            double[,] mat = new double[2, 3];
+
+            //Propriedades: Length, Rank e GetLength
+            Console.WriteLine(mat.Length);
+
+            //Rank: verifica quanto é a primeira dimensão da matriz (linhas) → 
+            Console.WriteLine(mat.Rank);
+
+            //GetLength(0) aponta para a primeira dimensão da matriz, quantidade de (linha) → 
+            Console.WriteLine(mat.GetLength(0));
+
+            //GetLength(1) aponta para a segunda dimensão da matriz, quantidade de (coluna) ↓ 
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
