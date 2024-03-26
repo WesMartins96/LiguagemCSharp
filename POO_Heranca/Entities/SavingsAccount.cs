44b31f4ee35309f512652d1b119b261fcf70a99f
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-26 07:45:43 -0300

    POO heranca sintaxe is e as

---
+++ POO_Heranca/Entities/SavingsAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POO_Heranca.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+            
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+    }
+}
---
+++ POO_Heranca/Program.cs
@@ -0,0 +1,50 @@
+﻿using POO_Heranca.Entities;
+using System;
+
+namespace POO_Heranca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Account account = new Account(1001, "Alex", 0.0);
+            BusinessAccount businessAccount = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+            // UPCASTING
+            Account account1 = businessAccount;
+            Account account2 = new BusinessAccount(1003, "Beatriz", 0.0, 200.0);
+            Account account3 = new SavingsAccount(1004, "Gael", 0.0, 0.01);
+
+            // DOWNCASTING  
+            BusinessAccount account4 = (BusinessAccount)account2;
+            account4.Loan(100.0);
+
+            //BusinessAccount account5 = (BusinessAccount)account3; erro de casting distapa em tempo de execução
+
+
+            // IS -> verificar se é de um tipo ou de outro
+            if (account3 is BusinessAccount)
+            {
+                //BusinessAccount account5 = (BusinessAccount)account3;
+                //AS -> outra forma de fazermos o casting
+                BusinessAccount account5 = account3 as BusinessAccount;
+                account5.Loan(200.0);
+                Console.WriteLine("Loan");
+            }
+
+            if (account3 is SavingsAccount)
+            {
+                //SavingsAccount account5 = (SavingsAccount)account3;
+                //AS -> outra forma de fazermos o casting
+                SavingsAccount account5 = account3 as SavingsAccount;
+                account5.UpdateBalance();
+                Console.WriteLine("Update!");
+            }
+
+
+
+
+        }
+    }
+}
