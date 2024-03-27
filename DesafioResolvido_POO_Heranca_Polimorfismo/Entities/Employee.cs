4f95a91b352039c241a50617381ff7193c5ed3ea
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-27 03:06:28 -0300

    POO desafio de codigo com heranca e polimorfismo

---
+++ DesafioResolvido_POO_Heranca_Polimorfismo/Entities/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioResolvido_POO_Heranca_Polimorfismo.Entities
+{
+    class Employee
+    {
+        public string Name { get; set; }
+        public int Hours { get; set; }
+        public double ValuePerHour { get; set; }
+
+        public Employee()
+        {
+            
+        }
+
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            Name = name;
+            Hours = hours;
+            ValuePerHour = valuePerHour;
+        }
+
+
+        //virtual: para liberar a operação Payment para que ela possa ser sobrescrita
+        public virtual double Payment()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
---
+++ DesafioResolvido_POO_Heranca_Polimorfismo/Program.cs
@@ -0,0 +1,71 @@
+﻿using DesafioResolvido_POO_Heranca_Polimorfismo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace DesafioResolvido_POO_Heranca_Polimorfismo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exercicío Resolvido
+            /*
+            Uma empresa possui funcionários próprios e terceirizados.
+            Para cada funcionário, deseja-se registrar nome, horas
+            trabalhadas e valor por hora. Funcionários terceirizados
+            possuem ainda uma despesa adicional.
+            O pagamento dos funcionários corresponde ao valor da hora
+            multiplicado pelas horas trabalhadas, sendo que os
+            funcionários terceirizados ainda recebem um bônus
+            correspondente a 110% de sua despesa adicional.
+            Fazer um programa para ler os dados de N funcionários (N
+            fornecido pelo usuário) e armazená-los em uma lista. Depois
+            de ler todos os dados, mostrar nome e pagamento de cada
+            funcionário na mesma ordem em que foram digitados.            
+             */
+            Console.WriteLine("--- Exercicío Resolvido ---");
+            Console.WriteLine();
+
+            CultureInfo culture = CultureInfo.InvariantCulture;
+            List<Employee> list = new List<Employee>();  
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), culture);
+ 
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = double.Parse(Console.ReadLine(), culture);
+                    list.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    list.Add(new Employee(name, hours, valuePerHour));
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("PAYMENTS:");
+            foreach (var emp in list)
+            {
+                Console.WriteLine($"{emp.Name} - $ {emp.Payment().ToString("F2", culture)}");
+            }
+
+
+            #endregion
+        }
+    }
+}
