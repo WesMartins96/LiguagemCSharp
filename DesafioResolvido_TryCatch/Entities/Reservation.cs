319782e16d385ae9c7219f05bf9e009a1d5b4c9c
Wesley Martins <wesley.mlopes@castgroup.com>
2024-03-28 17:01:01 -0300

    Iniciando o desafio resolvido para fixacao

---
+++ DesafioResolvido_TryCatch/Entities/Reservation.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioResolvido_TryCatch.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+
+        public Reservation()
+        {
+            
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            //pegamos a duracao entre a entrada(checkIn) e a saida(CheckOut)
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"Reservation: Room {RoomNumber}, check-in: {CheckIn.ToString("dd/MM/yyyy")}, check-out: {CheckOut.ToString("dd/MM/yyyy")}, {Duration()} nights");
+        }
+
+
+    }
+}
---
+++ DesafioResolvido_TryCatch/Program.cs
@@ -0,0 +1,67 @@
+﻿using DesafioResolvido_TryCatch.Entities;
+using System;
+
+namespace DesafioResolvido_TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Exercicio 
+            /*Fazer um programa para ler os dados de uma reserva de hotel (número do quarto, data
+            de entrada e data de saída) e mostrar os dados da reserva, inclusive sua duração em
+            dias. Em seguida, ler novas datas de entrada e saída, atualizar a reserva, e mostrar
+            novamente a reserva com os dados atualizados. O programa não deve aceitar dados
+            inválidos para a reserva, conforme as seguintes regras:
+            - Alterações de reserva só podem ocorrer para datas futuras
+            - A data de saída deve ser maior que a data de entrada
+             */
+            Console.WriteLine("--- Exercicio ---");
+            Console.WriteLine();
+
+
+            Console.Write("Room number: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Check-in date (dd/MM/yyyy): ");
+            DateTime checkIn = DateTime.Parse(Console.ReadLine());
+            Console.Write("Check-out date (dd/MM/yyyy): ");
+            DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+            if (checkOut <= checkIn)
+            {
+                Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+            }
+            else
+            {
+                Reservation reservation = new Reservation(roomNumber, checkIn, checkOut);
+                Console.WriteLine("Reservation:" + reservation);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation:");
+
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                DateTime now = DateTime.Now;
+                if (checkOut < now || checkIn < now)
+                {
+                    Console.WriteLine("Error in reservation: Reservation dates for update must be future dates");
+                }
+                else if (checkOut <= checkIn)
+                {
+                    Console.WriteLine("Error in reservation: Check-out date must be after check-in date");
+                }
+                else
+                {
+                    reservation.UpdateDates(checkIn, checkOut);
+                    Console.WriteLine("Reservation:" + reservation);
+                }              
+
+            }
+
+            #endregion
+        }
+    }
+}
