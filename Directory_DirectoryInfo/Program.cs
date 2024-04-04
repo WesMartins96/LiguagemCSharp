67c50139742286fb2ed6f9e0937f2bc8b7bd4b79
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-04 03:05:28 -0300

    mais sobre manipulacao de arquivos

---
+++ Directory_DirectoryInfo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Directory_DirectoryInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Teste_Aulas\myfolder";
+
+            try
+            {
+                //listar todas as pastas a partir de uma pasta informada
+                IEnumerable<string> folders = Directory.EnumerateDirectories(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FOLDERS: ");
+                foreach (string folder in folders)
+                {
+                    Console.WriteLine(folder);
+                }
+
+                //Listar os arquivos a partir de uma pasta informada
+                IEnumerable<string> files = Directory.EnumerateFiles(path, "*.*", SearchOption.AllDirectories);
+                Console.WriteLine("FILES: ");
+                foreach (string file in files)
+                {
+                    Console.WriteLine(file);
+                }
+
+                //Criar uma pasta
+                Directory.CreateDirectory(path + @"\newfolder");
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro!");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+}
---
+++ Path/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Path_Arquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Teste_Aulas\myfolder\file1.txt";
+
+            try
+            {
+                Console.WriteLine("DirectorySeparatorChar: " + Path.DirectorySeparatorChar);
+                Console.WriteLine("PathSeparator: " + Path.PathSeparator);
+                Console.WriteLine("GetDirectoryName: " + Path.GetDirectoryName(path));
+                Console.WriteLine("GetFileName: " + Path.GetFileName(path));
+                Console.WriteLine("GetExtension: " + Path.GetExtension(path));
+                Console.WriteLine("GetFileNameWithoutExtension: " + Path.GetFileNameWithoutExtension(path));
+                Console.WriteLine("GetFullPath: " + Path.GetFullPath(path));
+                Console.WriteLine("GetTempPath: " + Path.GetTempPath());
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
