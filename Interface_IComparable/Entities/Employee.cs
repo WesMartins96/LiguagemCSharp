9fe83191349ce444fba3013777e941b30c99b351
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-04 08:40:32 -0300

    utilizando o Icomparable e compareTo

---
+++ Interface_IComparable/Entities/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Interface_IComparable.Entities
+{
+    class Employee : IComparable
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            Name = vect[0];
+            Salary = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        //Comparação
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Employee))
+            {
+                throw new ArgumentException("Erro ao comparar!");
+            }
+            Employee other = obj as Employee;
+            return Name.CompareTo(other.Name);
+        }
+    }
+}
---
+++ Interface_IComparable/Program.cs
@@ -0,0 +1,38 @@
+﻿using Interface_IComparable.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Interface_IComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+              
+            string path = @"C:\Teste_Aulas\myfolder\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<Employee> list = new List<Employee>();
+                    while (!sr.EndOfStream)
+                    {
+                        list.Add(new Employee(sr.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach (var emp in list)
+                    {
+                        Console.WriteLine(emp);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Ocorreu um erro!");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
