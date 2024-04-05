59ac6bf60a70d89ba527f9f645dbfcafe15bfb22
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-05 01:15:24 -0300

    usando generics e IComparable, com restricoes de where

---
+++ Desafio_Resolvido_RestricaoGenerics/Entities/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Resolvido_RestricaoGenerics.Entities
+{
+    class Product : IComparable
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Product))
+            {
+                throw new ArgumentException("Erro ao comparar: argumneto não é Product");
+            }
+            Product other = obj as Product;
+            return Price.CompareTo(other.Price);
+        }
+    }
+}
---
+++ Desafio_Resolvido_RestricaoGenerics/Program.cs
@@ -0,0 +1,37 @@
+﻿using Desafio_Resolvido_RestricaoGenerics.Entities;
+using Desafio_Resolvido_RestricaoGenerics.Service;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Desafio_Resolvido_RestricaoGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter N: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] vect = Console.ReadLine().Split(',');  
+                string name = vect[0];
+                double price = double.Parse(vect[1], CultureInfo.InvariantCulture);
+                
+                list.Add(new Product(name, price));
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            Product max = calculationService.Max(list);
+
+            Console.WriteLine("Most Expansive: ");
+            Console.WriteLine(max);
+
+        }
+    }
+}
