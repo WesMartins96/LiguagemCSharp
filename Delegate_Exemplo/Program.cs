18ce0c8dabbb224290ce6f0a6441214502a0f0c6
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-08 08:30:18 -0300

    introducao a Delegates

---
+++ Delegate_Exemplo/Program.cs
@@ -0,0 +1,23 @@
+﻿using Delegate_Exemplo.Services;
+using System;
+
+namespace Delegate_Exemplo
+{
+
+    delegate double BinaryNumericOperation(double n1, double n2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+
+            double result = op(a, b);
+            Console.WriteLine(result);
+
+        }
+    }
+}
