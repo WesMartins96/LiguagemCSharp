8578a176ca0e0af4f85c6206bbaadcade90b38b3
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-09 04:42:37 -0300

    multicast delegates

---
+++ Multicast_Delegates/Program.cs
@@ -0,0 +1,23 @@
+﻿using Multicast_Delegates.Services;
+using System;
+
+namespace Multicast_Delegates
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax;
+
+            op.Invoke(a, b);
+
+
+        }
+    }
+}
---
+++ Multicast_Delegates/Services/CalculationService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multicast_Delegates.Services
+{
+    class CalculationService
+    {
+        public static void ShowMax(double x, double y)
+        {
+            double max = (x > y) ? x : y;
+            Console.WriteLine("Max: " + max);
+        }
+
+        public static void ShowSum(double x, double y)
+        {
+            double sum = x + y;
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
