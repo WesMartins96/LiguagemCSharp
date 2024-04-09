ce24e9a586e5d5de84f8bcaf4dc6d2a1601fb796
Wesley Martins <wesley.mlopes@castgroup.com>
2024-04-09 08:27:54 -0300

    introdução ao LINQ e suas querys

---
+++ Linq_ExemploDeUso/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Linq_ExemploDeUso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //expecificar o datasouce (fonte de dados)
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, };
+
+            //definir a query(consulta)
+            var result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+            //executa a query(consulta)
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
